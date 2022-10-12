a4130dc78e1d933b88c08d940cb73fffdddd0d02
Kiran Gandhi <kiran.gandhi@1Rintern.local>
2022-10-12 11:26:35 +0530

    routing

--- CMS-Projects/src/app/app-routing.module.ts
+++ CMS-Projects/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [{ path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'company'
+  },
+  {
+    path: 'company',
+    loadChildren: () => import('./company/company.module').then(m => m.CompanyModule)
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CMS-Projects/src/app/company/company-routing.module.ts
+++ CMS-Projects/src/app/company/company-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent }];
+const routes: Routes = [
+  { 
+    path: '',
+     component: CompanyComponent,
+     children:[
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'add'
+      },
+      {
+        path:'add',
+        component:CompanyFormComponent
+      },
+      {
+        path:'edit/:id',
+        component:CompanyFormComponent
+      }
+     ]
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
