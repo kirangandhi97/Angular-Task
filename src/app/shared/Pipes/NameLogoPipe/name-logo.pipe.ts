7c1c4141c6e6fe885ca88648aacc422c5d86ba6b
Kiran97 <kiran97@Kiran-Gandhis-MacBook-Air.local>
2022-10-16 16:36:30 +0530

    name-logo pipr done

--- src/app/shared/Pipes/NameLogoPipe/name-logo.pipe.ts
+++ src/app/shared/Pipes/NameLogoPipe/name-logo.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NameLogoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    value = value.toUpperCase();
+    const result = value.split(' ');
+    return `${result[0].charAt(0)}${result[1].charAt(0)}`;
+    
+    
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogoPipePipe } from './Pipes/logo-pipe.pipe';
 import { AlphabetOnlyDirective } from './directives/alphabet-only.directive';
 import { NameLogoPipe } from './Pipes/NameLogoPipe/name-logo.pipe';
-import { SearchItemPipe } from './Pipes/NameLogoPipe/search-item.pipe';
+import { SearchCompanyPipe } from './Pipes/searchCompanyPipe/search-company.pipe';
 
 
 
 @NgModule({
   declarations: [
-    LogoPipePipe,
     AlphabetOnlyDirective,
     NameLogoPipe,
-    SearchItemPipe
+    SearchCompanyPipe
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    NameLogoPipe,
+    SearchCompanyPipe
   ]
 })
 export class SharedModule { }
